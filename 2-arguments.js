ea251a763f0a1bf0bf5610ffc70c0518ddb87552
Adaking-hub <obi.chioma478@gmail.com>
2025-07-11 09:52:29 +0100

    Javascript is amazing

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,4 @@
-//Accept arguments and print them
+//Accept arguments and print their count
 
 const argsNum = process.argv.slice(2);
 
@@ -6,9 +6,12 @@ const argsNum = process.argv.slice(2);
 if (argsNum.length === 0) {
   console.log("No argument");
 }
+
+//if one argument is passed, print "Argument found"
 else if (argsNum.length === 1) {
   console.log("Argument found");
 }
+//if more than one argument is passed, print "Arguments found"
 else {
     console.log("Arguments found");
 }
---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let result = '';
+let i = 0;
+
+while (i < lines.length) {
+  result += lines[i] + '\n';
+  i++;
+}
+
+console.log(result.trim());
