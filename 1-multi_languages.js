19a6c24fc2f2a520577aed70c5476bcc72ae53c4
Adaking-hub <obi.chioma478@gmail.com>
2025-07-11 04:34:40 +0100

    Javascript is amazing

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+//print three lines to the console
+const cLanguage = "C is fun";
+const pythonLanguage = "Python is cool";
+const jsLanguage = "JavaScript is amazing";
+
+
+console.log(cLanguage);
+console.log(pythonLanguage);
+console.log(jsLanguage);
