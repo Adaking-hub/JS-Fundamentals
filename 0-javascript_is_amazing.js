e3c4588e6af011a47c0a71a1143ae2373b9541f5
Adaking-hub <obi.chioma478@gmail.com>
2025-07-11 04:29:49 +0100

    Javascript is amazing

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+//First constant, first print
+const myVar = "JavaScript is amazing";
+
+// Print the variable to the console
+console.log(myVar);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JS-Fundamentals
+
+In this project, I will be exploring the different power of javascript
