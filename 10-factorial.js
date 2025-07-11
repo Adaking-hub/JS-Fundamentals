cbd60528f59f9f1457dde3d25e9e150e54b03544
Adaking-hub <obi.chioma478@gmail.com>
2025-07-11 11:03:21 +0100

    Javascript is amazing

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+//compute the factorial of a number
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+function factorial(n) {
+  if (isNaN(n) || n < 0) return 1;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
